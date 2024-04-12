64a4a6e9991658e1732066b34649d67a5f424a11
Kartiksinh <raakartiksinh@gmail.com>
2024-04-12 17:59:48 +0530

    interview Questions file uplod

---
+++ Interview_Questions_JS/LeetCode.js
@@ -0,0 +1,83 @@
+//Pro-1
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+
+// Example 1:
+
+// let  nums = [3,2,4,10,12];
+
+
+
+// var nums = [2, 7, 11, 15];
+// var target = 13;
+
+
+// var i = 0;
+// var j = i + 1;
+
+// while (i < nums.length && j < nums.length) {
+
+//     if (nums[i] + nums[j] === target) {
+//         Result.push(i, j)
+//         console.log(Result)
+
+//     }
+//     i++;
+//     j++;
+
+// }
+
+
+
+
+
+// var nums = [3,2,4];
+// var target = 6;
+
+// var nums = [3,3];
+// var target = 6;
+
+
+
+
+
+
+// var nums = [2, 7, 11, 15];
+// var target = 13;
+
+// function twoSum(nums, target) {
+//   for (var i = 0; i < nums.length; i++) {
+//     for (var j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return [];
+// }
+
+// var result = twoSum(nums, target);
+
+// if (result.length === 2) {
+//   console.log(`Indices of the two numbers that add up to ${target}: [${result}]`);
+// } else {
+//   console.log("No such pair found.");
+// }
+
+
+// let a = "Tarun"
+// a = a + "Rudakiya"
+
+// console.log(a)
+// var a = 10
+//  a++
+//  console.log(a)
+
+//  ++a
+
+// console.log(a);
+
+
